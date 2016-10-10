fa54784cd42ff09f0f09b29cd7f756bd3d75cc34
AInotamm <rondonay@gmail.com>
2016-10-10 20:07:30 +0800

    fix detail css

--- Resources/Views/detail.blade.php
+++ Resources/Views/detail.blade.php
@@ -5,7 +5,7 @@
 	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">    
 	<title>失物招领-详细信息</title>
-	<link rel="stylesheet" href="static/css/detail.css">
+	<link rel="stylesheet" href="/lostandfound/css/detail.css">
 </head>
 <body>
 	<p class = "baseInfo">
